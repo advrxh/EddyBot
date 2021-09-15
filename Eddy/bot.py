3a71cf4f99159ffc2cf909dfccacfe25a28bd356
aadilvarsh <aadilvarshofficial@gmail.com>
2021-09-15 23:09:54 +0530

    update dependencies 📑

--- Eddy/bot.py
+++ Eddy/bot.py
@@ -1,21 +1,25 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from discord.ext import commands
 import discord
 import config
 
+
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('pls ed'), **kwargs)
+        super().__init__(command_prefix=commands.when_mentioned_or("pls ed "), **kwargs)
         for cog in config.cogs:
             try:
                 self.load_extension(cog)
             except Exception as exc:
-                print('Could not load extension {0} due to {1.__class__.__name__}: {1}'.format(cog, exc))
+                print(
+                    "Could not load extension {0} due to {1.__class__.__name__}: {1}".format(
+                        cog, exc
+                    )
+                )
 
     async def on_ready(self):
-        print('Logged on as {0} (ID: {0.id})'.format(self.user))
+        print("Logged on as {0} (ID: {0.id})".format(self.user))
 
 
 bot = Bot()
